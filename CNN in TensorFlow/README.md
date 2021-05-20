8d1bb8c02d6e78d3efe111e09dccd8ad6f614c30
NguyenLinh <linhmt100899@gmail.com>
2021-05-20 15:20:35 +0700

    image augmentation use ImageDataGenerator

--- CNN in TensorFlow/README.md
+++ CNN in TensorFlow/README.md
@@ -22,3 +22,8 @@ python3 train.py
 ```
 python3 predict.py 
 ```
+# data augmentation
+- Image augmentation and data augmentation is one of the most widely used tools in deep learning to increase dataset size and make neural networks perform better
+- Augmentation simply amends images on-the-fly while training using transforms like rotation, scale, flip, ...
+- After use Image augmentation, increase accuracy from 77% to 83%. Visualize accuracy and loss per epoch
+
--- CNN in TensorFlow/train.py
+++ CNN in TensorFlow/train.py
@@ -7,25 +7,31 @@ import os
 import cv2
 import numpy as np
 
+BATCH_SIZE = 64
+IMAGE_SIZE = 64
+EPOCHS = 20
+
 train_dir = os.path.join("dogs-vs-cats/data", 'train')
 test_dir = os.path.join("dogs-vs-cats/data", 'test')
 
 ## Preprocessing data
-# All image will be rescale by 1.0/255.
-train_datagen = ImageDataGenerator(rescale=1.0/255.)
+# All image will be rescale by 1.0/255, rotate image, zoom, flip, ...
+train_datagen = ImageDataGenerator(rescale=1.0/255., rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,
+                                    zoom_range=0.2, horizontal_flip=True, fill_mode='nearest'
+)
 test_datagen = ImageDataGenerator(rescale=1.0/255.)
 # Flow from directory training and test image in batch of 32, resize image with shape (64,64)
 # Format of data can use flow_from_directory:
 # - train_dir
 #   - cat
 #   - dog  
-train_generator = train_datagen.flow_from_directory(train_dir, target_size=(64,64), class_mode='binary', batch_size=32)
-test_generator = test_datagen.flow_from_directory(test_dir, target_size=(64,64), class_mode='binary', batch_size=32)
-
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(IMAGE_SIZE,IMAGE_SIZE), class_mode='binary', batch_size=BATCH_SIZE)
+test_generator = test_datagen.flow_from_directory(test_dir, target_size=(IMAGE_SIZE,IMAGE_SIZE), class_mode='binary', batch_size=BATCH_SIZE)
+print(len(train_generator))
 ## define model
 model = tf.keras.models.Sequential([
     # input shape 64x64 with 3 bytes color
-    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(64, 64, 3)),
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3)),
     tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2), 
@@ -35,7 +41,7 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(), 
     # 512 neuron hidden layer
     tf.keras.layers.Dense(512, activation='relu'), 
-    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class 'cats' and 1 for the other 'dogs'
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('cats') and 1 for the other ('dogs')
     tf.keras.layers.Dense(1, activation='sigmoid')  
 ])
 print(model.summary())
@@ -50,7 +56,7 @@ model.compile(optimizer=RMSprop(lr=0.001),
 filepath = 'best_acc.h5'
 callbacks = tf.keras.callbacks.ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
 ## Training model with 20 epochs
-history = model.fit_generator(train_generator, validation_data=test_generator, steps_per_epoch=100, callbacks=[callbacks], epochs=20, validation_steps=50,verbose=2)
+history = model.fit_generator(train_generator, validation_data=test_generator, steps_per_epoch=len(train_generator), callbacks=[callbacks], epochs=EPOCHS, validation_steps=len(test_generator), workers=6, use_multiprocessing=True)
 
 ## Visualize accuracy and loss
 # Plot training and validation accuracy per epoch
